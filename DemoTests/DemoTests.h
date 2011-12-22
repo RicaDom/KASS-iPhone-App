c76f2fa1e47dd72ab80f0507bb6451bc31a05329
zhicai <wesley.wang@Wesley-Wangs-MacBook-Pro.local>
2011-12-22 09:32:27 -0800

    Initial Commit

---
+++ DemoTests/DemoTests.h
@@ -0,0 +1,13 @@
+//
+//  DemoTests.h
+//  DemoTests
+//
+//  Created by zhicai on 12/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DemoTests : SenTestCase
+
+@end
---
+++ DemoTests/DemoTests.m
@@ -0,0 +1,32 @@
+//
+//  DemoTests.m
+//  DemoTests
+//
+//  Created by zhicai on 12/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DemoTests.h"
+
+@implementation DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DemoTests");
+}
+
+@end
