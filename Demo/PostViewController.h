9e90f11895edaee31fa84d856bfa35a4a8ca0476
wesley.wang <zhicaiwang@gmail.com>
2012-01-17 17:14:58 +0800

    Update post flow scroll view

--- Demo/PostViewController.h
+++ Demo/PostViewController.h
@@ -9,12 +9,18 @@
 #import <UIKit/UIKit.h>
 #import "AppScrollView.h"
 
-@interface PostViewController : UIViewController <UIScrollViewDelegate> 
+@interface PostViewController : UIViewController <UIScrollViewDelegate> {
+    @private
+    BOOL hotPostPageControlBeingUsed;
+    BOOL editorPostPageControlBeingUsed;
+}
 
 @property (weak, nonatomic) IBOutlet AppScrollView *hotPostScrollView;
-
 @property (weak, nonatomic) IBOutlet UIPageControl *hotPostPageControl;
+@property (weak, nonatomic) IBOutlet UIScrollView *editorPostScrollView;
+@property (weak, nonatomic) IBOutlet UIPageControl *editorPostPageControl;
 
+- (IBAction)changeEditorPostPage;
 - (IBAction)changeHotPostPage;
 //- (void) touchesEnded: (NSSet *) touches withEvent: (UIEvent *) event;
 @end
--- Demo/PostViewController.m
+++ Demo/PostViewController.m
@@ -11,6 +11,8 @@
 @implementation PostViewController
 @synthesize hotPostScrollView;
 @synthesize hotPostPageControl;
+@synthesize editorPostScrollView;
+@synthesize editorPostPageControl;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,24 +33,67 @@
 
 - (void)scrollViewDidScroll:(UIScrollView *)sender {
     NSLog(@"Im in");
-    // Update the page when more than 50% of the previous/next page is visible
-//    CGFloat pageWidth = self.scrollView.frame.size.width;
-//    int page = floor((self.scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
-//    self.pageControl.currentPage = page;
+    if (sender == self.hotPostScrollView) {
+        if (!hotPostPageControlBeingUsed) {
+            // Switch the indicator when more than 50% of the previous/next page is visible
+            CGFloat pageWidth = self.hotPostScrollView.frame.size.width;
+            int page = floor((self.hotPostScrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+            self.hotPostPageControl.currentPage = page;
+        }
+    } else if (sender == self.editorPostScrollView) {
+        if (!editorPostPageControlBeingUsed) {
+            // Switch the indicator when more than 50% of the previous/next page is visible
+            CGFloat pageWidth = self.editorPostScrollView.frame.size.width;
+            int page = floor((self.editorPostScrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+            self.editorPostPageControl.currentPage = page;
+        }
+    }	
 }
 
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
-    NSLog(@"Im a");
-	//pageControlBeingUsed = NO;
+    if (scrollView == self.hotPostScrollView) {
+        hotPostPageControlBeingUsed = NO;
+    } else if (scrollView == self.editorPostScrollView) {
+        editorPostPageControlBeingUsed = NO;
+    }	
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    NSLog(@"Im e");
-	//pageControlBeingUsed = NO;
+    if (scrollView == self.hotPostScrollView) {
+        hotPostPageControlBeingUsed = NO;
+    } else if (scrollView == self.editorPostScrollView) {
+        editorPostPageControlBeingUsed = NO;
+    }	
+}
+
+- (IBAction)changeEditorPostPage {
+    // Update the scroll view to the appropriate page
+	CGRect frame;
+	frame.origin.x = self.editorPostScrollView.frame.size.width * self.editorPostPageControl.currentPage;
+	frame.origin.y = 0;
+	frame.size = self.editorPostScrollView.frame.size;
+	[self.editorPostScrollView scrollRectToVisible:frame animated:YES];
+	
+	// Keep track of when scrolls happen in response to the page control
+	// value changing. If we don't do this, a noticeable "flashing" occurs
+	// as the the scroll delegate will temporarily switch back the page
+	// number.
+	editorPostPageControlBeingUsed = YES;    
 }
 
 - (IBAction)changeHotPostPage {
-    NSLog(@"Im eaction");
+    // Update the scroll view to the appropriate page
+	CGRect frame;
+	frame.origin.x = self.hotPostScrollView.frame.size.width * self.hotPostPageControl.currentPage;
+	frame.origin.y = 0;
+	frame.size = self.hotPostScrollView.frame.size;
+	[self.hotPostScrollView scrollRectToVisible:frame animated:YES];
+	
+	// Keep track of when scrolls happen in response to the page control
+	// value changing. If we don't do this, a noticeable "flashing" occurs
+	// as the the scroll delegate will temporarily switch back the page
+	// number.
+	hotPostPageControlBeingUsed = YES;
 }
 
 - (void) touchesEnded: (NSSet *) touches withEvent: (UIEvent *) event 
@@ -96,6 +141,14 @@
     //return [super hitTest:point withEvent:event];
 }
 
+- (void)singleTapGestureCaptured:(UITapGestureRecognizer *)gesture
+{ 
+    //CGPoint touchPoint=[gesture locationInView:hotPostScrollView];
+    
+    [self performSegueWithIdentifier:@"startPostFlowSegue" sender:self];
+    NSLog(@"The page is %@", [NSNumber numberWithInt:self.hotPostPageControl.currentPage]);
+}
+
 //- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
 //    if ([segue.identifier isEqualToString:@"loadPostSegue"]) {
 //       // PostFlowViewController *pvc = [segue destinationViewController];
@@ -126,25 +179,82 @@
 {
     [super viewDidLoad];
     
-	//pageControlBeingUsed = NO;
-	
-	NSArray *colors = [NSArray arrayWithObjects:[UIColor redColor], [UIColor greenColor], [UIColor blueColor], nil];
-	for (int i = 0; i < colors.count; i++) {
-		CGRect frame;
-		frame.origin.x = self.hotPostScrollView.frame.size.width * i;
-		frame.origin.y = 0;
-		frame.size = self.hotPostScrollView.frame.size;
-		
-		UIView *subview = [[UIView alloc] initWithFrame:frame];
-		subview.backgroundColor = [colors objectAtIndex:i];
-		[self.hotPostScrollView addSubview:subview];
-	}
-	
-	self.hotPostScrollView.contentSize = CGSizeMake(self.hotPostScrollView.frame.size.width * colors.count, self.hotPostScrollView.frame.size.height);
+    // HOT POST EXAMPLES:
+	hotPostPageControlBeingUsed = NO;	
+//	NSArray *colors = [NSArray arrayWithObjects:[UIColor redColor], [UIColor greenColor], [UIColor blueColor], nil];
+//	for (int i = 0; i < colors.count; i++) {
+//		CGRect frame;
+//		frame.origin.x = self.hotPostScrollView.frame.size.width * i;
+//		frame.origin.y = 0;
+//		frame.size = self.hotPostScrollView.frame.size;
+//		
+//		UIView *subview = [[UIView alloc] initWithFrame:frame];
+//		subview.backgroundColor = [colors objectAtIndex:i];
+//		[self.hotPostScrollView addSubview:subview];
+//	}
+    CGRect frame;
+    frame.origin.x = self.hotPostScrollView.frame.size.width * 0;
+    frame.origin.y = 0;
+    frame.size = self.hotPostScrollView.frame.size;
+    
+    UIView *subview = [[UIView alloc] initWithFrame:frame];
+    subview.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"Zaarly_logo3-300x143.png"]];
+    [self.hotPostScrollView addSubview:subview];
+
+    frame.origin.x = self.hotPostScrollView.frame.size.width * 1;
+    frame.origin.y = 0;
+    frame.size = self.hotPostScrollView.frame.size;
+    subview = [[UIView alloc] initWithFrame:frame];
+    subview.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"martin_luther_king-2012-hp.png"]];
+    [self.hotPostScrollView addSubview:subview];    
+
+    frame.origin.x = self.hotPostScrollView.frame.size.width * 2;
+    frame.origin.y = 0;
+    frame.size = self.hotPostScrollView.frame.size;
+    subview = [[UIView alloc] initWithFrame:frame];
+    subview.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"images.png"]];
+    [self.hotPostScrollView addSubview:subview]; 
+    
+	self.hotPostScrollView.contentSize = CGSizeMake(self.hotPostScrollView.frame.size.width * 3, self.hotPostScrollView.frame.size.height);
 	
 	self.hotPostPageControl.currentPage = 0;
-	self.hotPostPageControl.numberOfPages = colors.count;
+	self.hotPostPageControl.numberOfPages = 3;
     
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTapGestureCaptured:)];
+    [hotPostScrollView addGestureRecognizer:singleTap]; 
+    
+    
+    // EDITOR POST EXAMPLES:
+	editorPostPageControlBeingUsed = NO;	
+    frame.origin.x = self.editorPostScrollView.frame.size.width * 0;
+    frame.origin.y = 0;
+    frame.size = self.editorPostScrollView.frame.size;
+    
+    subview = [[UIView alloc] initWithFrame:frame];
+    subview.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"Zaarly_logo3-300x143.png"]];
+    [self.editorPostScrollView addSubview:subview];
+    
+    frame.origin.x = self.editorPostScrollView.frame.size.width * 1;
+    frame.origin.y = 0;
+    frame.size = self.editorPostScrollView.frame.size;
+    subview = [[UIView alloc] initWithFrame:frame];
+    subview.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"martin_luther_king-2012-hp.png"]];
+    [self.editorPostScrollView addSubview:subview];    
+    
+    frame.origin.x = self.editorPostScrollView.frame.size.width * 2;
+    frame.origin.y = 0;
+    frame.size = self.editorPostScrollView.frame.size;
+    subview = [[UIView alloc] initWithFrame:frame];
+    subview.backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:@"images.png"]];
+    [self.editorPostScrollView addSubview:subview]; 
+    
+	self.editorPostScrollView.contentSize = CGSizeMake(self.editorPostScrollView.frame.size.width * 3, self.editorPostScrollView.frame.size.height);
+	
+	self.editorPostPageControl.currentPage = 0;
+	self.editorPostPageControl.numberOfPages = 3;
+    
+    singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTapGestureCaptured:)];
+    [editorPostScrollView addGestureRecognizer:singleTap];     
     NSLog(@"Self = %@", self);
    // NSLog(@"Current responder = %@", [self.view findFirstResponder]);
 }
@@ -155,6 +265,8 @@
     [self setHotPostScrollView:nil];
     [self setHotPostPageControl:nil];
     [self setHotPostScrollView:nil];
+    [self setEditorPostScrollView:nil];
+    [self setEditorPostPageControl:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
