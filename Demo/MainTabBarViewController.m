2fc2b1989deb461deb309589483fbdc79a3c7cf0
Qi He <qihe229@gmail.com>
2012-01-25 18:02:29 +0800

    ...

--- Demo/MainTabBarViewController.m
+++ Demo/MainTabBarViewController.m
@@ -62,7 +62,7 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {   
-  NSLog(@"MainTabBarViewController::viewDidLoad ");\
+  NSLog(@"MainTabBarViewController::viewDidLoad ");
   [super viewDidLoad];
 }
 
