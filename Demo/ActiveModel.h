80d9776244e15290d8326e86b17075a47a131371
Qi He <qihe229@gmail.com>
2012-03-07 15:55:59 +0800

    segue

---
+++ Demo/ActiveModel.h
@@ -0,0 +1,13 @@
+//
+//  ActiveModel.h
+//  Demo
+//
+//  Created by Qi He on 12-3-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ActiveModel : NSObject
+
+@end
---
+++ Demo/ActiveModel.m
@@ -0,0 +1,13 @@
+//
+//  ActiveModel.m
+//  Demo
+//
+//  Created by Qi He on 12-3-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ActiveModel.h"
+
+@implementation ActiveModel
+
+@end
