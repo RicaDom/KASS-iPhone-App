c16cf20f29570e45b5ad0072ec598b8a680c5d9d
Qi He <qihe229@gmail.com>
2012-03-08 20:29:43 +0800

    allow push notification and get token

--- Demo/AppDelegate.m
+++ Demo/AppDelegate.m
@@ -18,7 +18,23 @@
   DLog(@"AppDelegate::didFinishLaunchingWithOptions:rootViewController=%@", self.window.rootViewController);
   [VariableStore.sharedInstance loadAndStoreSettings:self];
   DLog(@"AppDelegate::doneLoading");
+  
+	// Let the device know we want to receive push notifications
+	[[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+   (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+  
   return YES;
+}
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+  //save the token, before that, we need to trim the spaces and get the correct token
+  [[NSUserDefaults standardUserDefaults] setValue:deviceToken forKey:KassAppIphoneTokenKey];    
+}
+
+- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
+{
+	
 }
 
 - (void)settingsDidLoad:(NSDictionary *)dict
