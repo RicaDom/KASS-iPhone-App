e8b9a7eefd1097fb73dfaf84e5fac5d7915af835
Qi He <qihe229@gmail.com>
2012-03-12 17:21:01 +0800

    price modifier and keyboard slider

---
+++ Demo/PriceModifier.m
@@ -0,0 +1,13 @@
+//
+//  PriceModifier.m
+//  Demo
+//
+//  Created by Qi He on 12-3-12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PriceModifier.h"
+
+@implementation PriceModifier
+
+@end
---
+++ Demo/UIViewController+PriceModifier.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+PriceModifier.m
+//  Demo
+//
+//  Created by Qi He on 12-3-12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIViewController+PriceModifier.h"
+
+@implementation UIViewController (PriceModifier)
+
+@end
